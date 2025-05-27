2d2e8ee4a999bcaf60480a51c5b5837faa9e0998
Evebaby93 <eala56847@gmail.com>
2025-05-27 15:52:25 +0100

    changes made

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,6 +1,6 @@
-let C = "C is fun"
-let Python = "Python is cool"
-let J = "JavaScript is amazing"
+Const C = "C is fun"
+Const Python = "Python is cool"
+Const J = "JavaScript is amazing"
 console.log("C")
 console.log("Python")
 console.log("J")
