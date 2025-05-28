c3ba0c73ca2206fd64f66c4ea642a9fe8c58bfb3
Evebaby93 <eala56847@gmail.com>
2025-05-28 11:16:56 +0100

    argv

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,2 +1,11 @@
+// console.log(process.argv[2])
+
+if (!process.argv[2]) {
 console.log("No argument")
+} else if (process.argv[2] && process.argv[3]) {
+    console.log("Arguments found")
+} else {
+    console.log("Argument found")
+}
+
 
