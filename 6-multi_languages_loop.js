2657f216f5e53bc0a31035054c65de9a659c7f32
Evelyn Alamba <eala56847@gmail.com>
2025-06-05 22:17:23 +0100

    Update 6-multi_languages_loop.js

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,5 +1,4 @@
 const str_arr = ["C is fun", "Python is cool", "JavaScript is amazing"]
-for(let i =0; i<str_arr.length; i++)
-{
-    console.log(str_arr[i])
-}
+str_arr.forEach((arr) => {
+    console.log(arr)
+})
