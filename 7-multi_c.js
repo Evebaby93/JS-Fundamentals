2c9804bc8937b332986dde1423013a00a4768343
Evelyn Alamba <eala56847@gmail.com>
2025-06-09 16:32:20 +0100

    Update 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,7 +1,7 @@
 let val = parseInt(process.argv[2])
 if(isNaN(val))
     console.log("Missing number of occurrences")
-while(val && val > 0) {
+while (val && val > 0) {
     console.log("C is fun")
     val--
 }
