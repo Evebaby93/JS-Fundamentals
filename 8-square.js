e75773b149bf4e5c6633ee41632ff32184f4415b
Evelyn Alamba <eala56847@gmail.com>
2025-06-09 16:39:58 +0100

    Update 8-square.js

--- 8-square.js
+++ 8-square.js
@@ -2,7 +2,7 @@ let val = parseInt(process.argv[2])
 const sq = val
 if(isNaN(val))
     console.log("Missing size")
-while(val && val > 0) {
+while (val && val > 0) {
     const txt = "X"
     console.log(txt.repeat(sq))
     val--
